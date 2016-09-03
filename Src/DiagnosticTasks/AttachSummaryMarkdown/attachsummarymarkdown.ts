3283d2e0cface187378d0ade3329f34533f4ff37
André Marques de Araújo <andremarques023@hotmail.com>
2016-09-03 20:38:53 -0300

    Tasks refactoring. Added class-based implementation.

--- Src/DiagnosticTasks/AttachSummaryMarkdown/attachsummarymarkdown.ts
+++ Src/DiagnosticTasks/AttachSummaryMarkdown/attachsummarymarkdown.ts
@@ -1,13 +1,15 @@
 import tl = require('vsts-task-lib/task');
 
-async function run() {
-    try {
-        let sectionName: string = tl.getInput('sectionName', true);
-        let markdownFilePath: string = tl.getPathInput('markdownFilePath', true)
-        console.log(`##vso[task.addattachment type=Distributedtask.Core.Summary;name=${sectionName};]${markdownFilePath}`);
-    } catch (error) {
-        tl.setResult(tl.TaskResult.Failed, error.message);
+export class AttachSummaryMarkdown {
+    public static async runTask() {
+        try {
+            let sectionName: string = tl.getInput('sectionName', true);
+            let markdownFilePath: string = tl.getPathInput('markdownFilePath', true);
+            console.log(`##vso[task.addattachment type=Distributedtask.Core.Summary;name=${sectionName};]${markdownFilePath}`);
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, err.message);
+        }
     }
 }
 
-run();
+AttachSummaryMarkdown.runTask();
